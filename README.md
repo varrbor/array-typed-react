f3e20352a776958914c82428c015d75f68c4051b
varrbor <varrbor@ukr.net>
2023-06-11 19:50:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# array-typed-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-wdhmjl)
---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+import './App.css'
+
+interface IInvoiceListProps {
+  invoiceData: InvoiceDataType;
+  logo?: string;
+}
+
+type InvoiceDataType = {
+  customerName: string;
+  invoices: {
+    id: number;
+    name: string;
+    total: string;
+  }[],
+  logo?: string;
+};
+
+const InvoiceList = (props: IInvoiceListProps) => {
+  const { customerName, invoices } = props.invoiceData;
+  return (
+    <div>
+      <h1>{customerName}</h1>
+      <hr />
+    </div>
+  );
+};
+
+const App = () => {
+  const data: InvoiceDataType = {
+    customerName: "Google",
+    invoices: [
+      { id: 123, name: "Dev work", total: "20.00", paymentStatus: "paid" },
+      {
+        id: 456,
+        name: "More Dev work",
+        total: "50.00",
+        paymentStatus: "pending",
+      },
+      {
+        id: 789,
+        name: "Something different",
+        total: "100.00",
+        paymentStatus: "paid",
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <InvoiceList invoiceData={data} />
+    </div>
+  );
+};
+
+export default App
